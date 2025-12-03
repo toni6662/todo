f46c58c62b1f63dc3c2d5025bba80bf1a7747b78
Anton Aynadzhiyski <anton.aynadzhiyski@gmail.com>
2025-12-03 14:07:55 +0200

    db fix

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,6 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+declare global {
+  // eslint-disable-next-line no-var
+  var _pgPool: InstanceType<typeof Pool> | undefined;
+}
+
 const pool = new Pool({
   host: 'p68dnc.h.filess.io',       // or your Docker container name
   port: 5432,
@@ -8,5 +13,10 @@ const pool = new Pool({
   password: 'fec19cf66218fdf41610f858f0673d8fa9737bf1',
   database: 'todo_announced',
 });
+
+
+if (process.env.NODE_ENV === "development") {
+  global._pgPool = pool;
+}
 
 export default pool;
