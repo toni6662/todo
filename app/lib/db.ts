c009306d2641ac5842b268811fbe4d9678681199
Anton Aynadzhiyski <anton.aynadzhiyski@gmail.com>
2025-12-03 15:53:44 +0200

    fix db connection error

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,12 +1,25 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: 'p68dnc.h.filess.io',       // or your Docker container name
-  port: 5432,
-  user: 'todo_announced',
-  password: 'fec19cf66218fdf41610f858f0673d8fa9737bf1',
-  database: 'todo_announced',
-});
+declare global {
+  var pgPool: InstanceType<typeof Pool> | undefined;
+}
+
+let pool: InstanceType<typeof Pool>;
+
+console.log('global.pgPool', global.pgPool);
+
+if(!global.pgPool) {
+  global.pgPool = new Pool({
+    host: 'p68dnc.h.filess.io',       // or your Docker container name
+    port: 5432,
+    user: 'todo_announced',
+    password: 'fec19cf66218fdf41610f858f0673d8fa9737bf1',
+    database: 'todo_announced',
+    max: 2
+  });
+}
+
+pool = global.pgPool;
 
 export default pool;
